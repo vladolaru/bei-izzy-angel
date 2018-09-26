b11824cbb8c93445ddb14d94cab6a493216cf422
anggabard <angel.ardeleanu@hotmail.com>
2018-09-26 12:22:35 +0300

    widgets (alpha version)

--- components/front-page-sidebars.php
+++ components/front-page-sidebars.php
@@ -3,8 +3,8 @@ register_sidebar(array(
 	'name' => 'Front-Page Widget 1',
 	'id'        => 'front-1',
 	'description' => 'First front-page widget area',
-	'before_widget' => '<div id="front-page-widget-1">',
-	'after_widget' => '</div>',
+	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+	'after_widget' => '</li>',
 	'before_title' => '<h2>',
 	'after_title' => '</h2>',
 ));
@@ -12,8 +12,8 @@ register_sidebar(array(
 	'name' => 'Front-Page Widget 2',
 	'id'        => 'front-2',
 	'description' => 'Second front-page widget area',
-	'before_widget' => '<div id="front-page-widget-2">',
-	'after_widget' => '</div>',
+	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+	'after_widget' => '</li>',
 	'before_title' => '<h2>',
 	'after_title' => '</h2>',
 ));
@@ -21,8 +21,8 @@ register_sidebar(array(
 	'name' => 'Front-Page Widget 3',
 	'id'        => 'front-3',
 	'description' => 'Third front-page widget area',
-	'before_widget' => '<div id="front-page-widget-3">',
-	'after_widget' => '</div>',
+	'before_widget' => '<li id="%1$s" class="widget %2$s">',
+	'after_widget' => '</li>',
 	'before_title' => '<h2>',
 	'after_title' => '</h2>',
 ));
--- front-page.php
+++ front-page.php
@@ -5,18 +5,22 @@
 get_header();
 ?>
 
-    <div id="primary" class="content-area main-content">
+    <div id="primary" class="content-area">
         <main id="main" class="site-main">
 
             <div id="front-widgets">
+                <ul id="front-widget-area-1">
 					<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'front-1' ) ) : ?>
 					<?php endif; ?>
-
+                </ul>
+                <ul id="front-widget-area-2">
 					<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'front-2' ) ) : ?>
 					<?php endif; ?>
-
+                </ul>
+                <ul id="front-widget-area-3">
 					<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'front-3' ) ) : ?>
-					<?php endif; ?>
+                    <?php endif; ?>
+                </ul>
             </div>
         </main><!-- #main -->
     </div><!-- #primary -->
